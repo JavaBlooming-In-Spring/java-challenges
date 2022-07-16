9dc154cade5fe659900a19410c0e21888237ab66
woo-chang <clllickme@naver.com>
2022-07-16 16:29:13 +0900

    test: 테스트 코드 작성

--- woo-chang/java_baseball/src/test/java/GeneratedTargetTest.java
+++ woo-chang/java_baseball/src/test/java/GeneratedTargetTest.java
@@ -1,21 +1,37 @@
-import static org.assertj.core.api.Assertions.*;
+import static org.assertj.core.api.Assertions.assertThat;
 
+import java.util.List;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 public class GeneratedTargetTest {
 
   @Test
-  @DisplayName("랜덤한 3자리 숫자 생성")
-  public void generateRandomNum() {
+  @DisplayName("랜덤 수 생성")
+  public void generatedTarget() {
     // given
     GeneratedTarget generatedTarget = new GeneratedTarget();
 
     // when
-    String target = generatedTarget.getTarget();
+    List<Integer> target = generatedTarget.getTarget();
 
     // then
-    assertThat(target.length()).isEqualTo(3);
+    assertThat(target.size()).isEqualTo(GameUtils.TARGET_LENGTH);
   }
+
+  @Test
+  @DisplayName("새로운 랜덤 수 생성")
+  public void generateNewTarget() {
+    // given
+    GeneratedTarget generatedTarget = new GeneratedTarget();
+
+    // when
+    generatedTarget.generatedNewTarget();
+    List<Integer> target = generatedTarget.getTarget();
+
+    // then
+    assertThat(target.size()).isEqualTo(GameUtils.TARGET_LENGTH);
+  }
+
 
 }
